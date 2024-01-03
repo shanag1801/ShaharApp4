e957b259324b10e421e1f6afb3f21c32894320ac
shanag1801 <shanag0110@gmail.com>
2024-01-03 16:15:23 +0200

    added the go function and made some changes to the button's design

--- app/src/main/java/com/example/shaharapp4/MainActivity.java
+++ app/src/main/java/com/example/shaharapp4/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.shaharapp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int n;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+    }
+    @SuppressLint("SetTextI18n")
+    public void go(View view){
+        n++;
+        btn.setText("This is a click number: " + n);
+        if(n % 7 == 0 || n % 10 == 7)
+            btn.setText("BOOM!");
     }
 }
